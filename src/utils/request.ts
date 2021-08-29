f3163ea09a5409cb07f375e8d13ea4a9a3e2f023
aline.chen <aline.chen@dbappsecurity.com.cn>
2021-08-29 22:17:41 +0800

    fix

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -7,6 +7,7 @@ import qs from 'qs'
 const request = axios.create({
   // 配置选项
   // timeout
+  baseURL: process.env.NODE_ENV === 'production'?'http://edufront.lagou.com':''
 })
 
 function redirectLogin () {
